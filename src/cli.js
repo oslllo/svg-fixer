c536931749d19d2d99fff395e76c9470a0a058fb
Ghustavh Ehm <ghustavh97@gmail.com>
2020-10-21 09:11:21 +0000

    feat: add CLI

---
+++ src/cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const SVGFixer = require("../");
+const colors = require("colors");
+var argvs = require("yargs/yargs")(process.argv.slice(2)); // eslint-disable-line no-magic-numbers
+
+argvs
+    .option("source", {
+        alias: "s",
+        describe: "path containing SVG(s).",
+        type: "string",
+        demandOption: true,
+    })
+    .option("destination", {
+        alias: "d",
+        describe: "path to store fixed SVG(s).",
+        type: "string",
+        demandOption: true,
+    })
+    .option("strict-destination", {
+        alias: "sd",
+        describe: "throw if destination path does not exist.",
+        default: true,
+        type: "boolean",
+    })
+    .option("show-progress", {
+        alias: "sp",
+        describe: "show progress bar in CLI.",
+        default: true,
+        type: "boolean",
+    })
+    .help();
+
+argvs = argvs.argv;
+
+const source = argvs["source"]; // eslint-disable-line dot-notation
+const destination = argvs["destination"]; // eslint-disable-line dot-notation
+const options = {
+    showProgressBar: argvs["show-progress"],
+    throwIfDestinationDoesNotExist: argvs["strict-destination"],
+};
+
+(async () => {
+    try {
+        await SVGFixer(source, destination, options).fix();
+    } catch (err) {
+        console.log("\n" + colors.red("ERR: " + err.message + "\n")); // eslint-disable-line no-console
+        console.log(colors.yellow("STACK: " + err.stack + "\n")); // eslint-disable-line no-console
+        process.exit(1); // eslint-disable-line no-process-exit, no-magic-numbers
+    }
+})();
---
+++ test/src/test.cli.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const fs = require("fs-extra");
+const { emptyDir, isEmptyDir } = require(".");
+const { exec } = require("child_process");
+const { assert, path2 } = require("./helper");
+
+var destination = "temp";
+
+if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+}
+
+describe("test.cli", () => {
+    beforeEach(() => {
+        emptyDir(destination);
+    });
+    it("works with direct path to a SVG", function (done) {
+        this.timeout(10000); // eslint-disable-line no-invalid-this
+        assert.isTrue(isEmptyDir(destination));
+        exec(
+            "src/cli.js -s " + path2.direct.relative + " -d " + destination,
+            (error, stdout, stderr) => {
+                if (error) {
+                    done(new Error(`error: ${error.message}`));
+                }
+                if (stderr) {
+                    done(new Error(`stderr: ${stderr}`));
+                }
+                assert.isFalse(isEmptyDir(destination));
+                assert.equal(fs.readdirSync(destination).length, 1);
+                done();
+            }
+        );
+    });
+    it("works with path to a directory containting multiple SVGs", function (done) {
+        this.timeout(10000); // eslint-disable-line no-invalid-this
+        assert.isTrue(isEmptyDir(destination));
+        exec(
+            "src/cli.js -s " + path2.double.relative + " -d " + destination,
+            (error, stdout, stderr) => {
+                if (error) {
+                    done(new Error(`error: ${error.message}`));
+                }
+                if (stderr) {
+                    done(new Error(`stderr: ${stderr}`));
+                }
+                assert.isFalse(isEmptyDir(destination));
+                assert.equal(fs.readdirSync(destination).length, 2);
+                done();
+            }
+        );
+    });
+    it("throws with invalid destination path", function (done) {
+        this.timeout(10000); // eslint-disable-line no-invalid-this
+        assert.isTrue(isEmptyDir(destination));
+        exec(
+            "src/cli.js -s ./invalid/destination -d " + destination,
+            (error, stdout) => {
+                assert.isTrue(error instanceof Error);
+                assert.isTrue(
+                    stdout.includes(
+                        "source path ./invalid/destination does not exist."
+                    )
+                );
+                assert.isTrue(isEmptyDir(destination));
+                done();
+            }
+        );
+    });
+});
